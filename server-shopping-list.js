3cf6b73a8ab3920640a1034d4813ef51eab2b5f9
Robert Frazier <robert.v.frazier@gmail.com>
2017-11-15 12:14:05 -0500

    Recipes get task done.

--- server-shopping-list.js
+++ server-shopping-list.js
@@ -1,11 +1,10 @@
-'use strict';
 
 const express = require('express');
 const router = express.Router();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
-const {Recipes} = require('./models');
+const {ShoppingList} = require('./models');
 
 const jsonParser = bodyParser.json();
 const app = express();
@@ -13,16 +12,16 @@ const app = express();
 // log the http layer
 app.use(morgan('common'));
 
-// we're going to add some items to Recipes
+// we're going to add some items to ShoppingList
 // so there's some data to look at
-Recipes.create('chocolate milk', ['cocoa', 'milk', 'sugar']);
-Recipes.create('egg mcmuffins', ['English muffins', 'eggs', 'Canadian bacon', 'cheese']);
-Recipes.create('spagetti', ['spagetti noodles', 'tomato sauce', 'tomato paste', 'garlic', 'basil']);
+ShoppingList.create('beans', 2);
+ShoppingList.create('tomatoes', 3);
+ShoppingList.create('peppers', 4);
 
 // when the root of this router is called with GET, return
-// all current Recipes items
-app.get('/Recipes', (req, res) => {
-  res.json(Recipes.get());
+// all current ShoppingList items
+app.get('/shopping-list', (req, res) => {
+  res.json(ShoppingList.get());
 });
 
 app.listen(process.env.PORT || 8080, () => {
